5e5f947e906f0c413c38d4de9d0f945668d68f2d
Matt O'Leary <mattosurf@gmail.com>
2021-05-05 11:08:57 -0700

    add favicon

--- .eleventy.js
+++ .eleventy.js
@@ -59,6 +59,7 @@ module.exports = function(eleventyConfig) {
   // Copy the `img` and `css` folders to the output
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("favicon_io")
 
   // Customize Markdown library and settings:
   let markdownLibrary = markdownIt({
